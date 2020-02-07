324819feda96629ec5b2140eeae0f10e822b9a01
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2020-02-07 15:18:45 -0500

    Tweaked the Positions in Main Menu
    The Game Entities are now in the correct positions in the Main Screen

--- PlayfulPicture/PlayfulPicture/MenuScreen.cpp
+++ PlayfulPicture/PlayfulPicture/MenuScreen.cpp
@@ -3,10 +3,10 @@
 MenuScreen::MenuScreen()
 {
 	//General Game Entity for the Menu and the Entities ontop of it
-	SelectOptions = new GameEntity(Graphics::SCREEN_WIDTH * 0.5f, Graphics::SCREEN_HEIGHT * 0.5f);
-	StartGameEntity = new GameEntity(100.0f, 100.0f);
-	MusicGameEntity = new GameEntity(100.0f, 100.0f);
-	QuitGameEntity = new GameEntity(100.0f, 100.0f);
+	SelectOptions = new GameEntity(Graphics::SCREEN_WIDTH, Graphics::SCREEN_HEIGHT);
+	StartGameEntity = new GameEntity(60.0f, 20.0f);
+	MusicGameEntity = new GameEntity(60.0f, 20.0f);
+	QuitGameEntity = new GameEntity(60.0f, 20.0f);
 
 	//Textures inside the Game Entities
 	StartTexture = new Texture("Start Game", "emulogic.ttf", 32, { 230, 230, 230 });
@@ -21,10 +21,12 @@ MenuScreen::MenuScreen()
 	QuitGameEntity->SetParent(SelectOptions);
 
 	StartTexture->SetParent(StartGameEntity);
-	MusicGameEntity->SetParent(MusicGameEntity);
-	QuitGameEntity->SetParent(QuitGameEntity);
+	MusicTexture->SetParent(MusicGameEntity);
+	QuitTexture->SetParent(QuitGameEntity);
 
 	//Set Positions
+	SelectOptions->SetPosition(Graphics::SCREEN_WIDTH * 0.5, Graphics::SCREEN_HEIGHT * 0.5);
+
 	StartGameEntity->SetPosition(0.0f, Graphics::SCREEN_HEIGHT * -0.3f);
 	MusicGameEntity->SetPosition(0.0f, 0.0f);
 	QuitGameEntity->SetPosition(0.0f, Graphics::SCREEN_HEIGHT * 0.3f);
