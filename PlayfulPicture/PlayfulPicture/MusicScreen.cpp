256e39a3b2eeebfbb0fedd67b99513683440bd5d
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-02-07 15:18:56 -0500

    Music screen complete except back button
    Music screen complete except for back button

--- PlayfulPicture/PlayfulPicture/MusicScreen.cpp
+++ PlayfulPicture/PlayfulPicture/MusicScreen.cpp
@@ -10,18 +10,22 @@ MusicScreen::MusicScreen()
 
 	button = new Buttons();
 
+	backgroundTexture = new Texture("Background.jpg", 0, 0, 1760, 1200);
+
 	iconHolder = new GameEntity(Graphics::SCREEN_WIDTH, Graphics::SCREEN_HEIGHT);
-	music1ButtonTexture = new Texture("Buttons.png", 344, 0, 172, 85);
+	music1ButtonTexture = new Texture("Buttons.png", 344, 85, 172, 85);
 	music2ButtonTexture = new Texture("Buttons.png", 516, 0, 172, 85);
 	music3ButtonTexture = new Texture("Buttons.png", 688, 0, 172, 85);
 	backButtonTexture = new Texture("Buttons.png", 172, 0, 172, 85);
 
-	//iconHolder->SetParent(this);
+	iconHolder->SetParent(this);
+	backgroundTexture->SetParent(iconHolder);
 	music1ButtonTexture->SetParent(iconHolder);
 	music2ButtonTexture->SetParent(iconHolder);
 	music3ButtonTexture->SetParent(iconHolder);
 	backButtonTexture->SetParent(iconHolder);
 
+	backgroundTexture->SetPosition(-Graphics::SCREEN_WIDTH *0.5f, -Graphics::SCREEN_HEIGHT*0.5f);
 	music1ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.75f, -Graphics::SCREEN_HEIGHT * 0.5f);
 	music2ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.5f);
 	music3ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.25f, -Graphics::SCREEN_HEIGHT * 0.5f);
@@ -63,6 +67,7 @@ MusicScreen* MusicScreen::Instance()
 
 void MusicScreen::Render()
 {
+	backgroundTexture->Render();
 	music1ButtonTexture->Render();
 	music2ButtonTexture->Render();
 	music3ButtonTexture->Render();
@@ -73,19 +78,61 @@ void MusicScreen::Update()
 {
 	if (mInput->MouseButtonPressed(mInput->Left)) {
 
-		if (button->ContainsPoint(music1ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y)) 
+		if (button->ContainsPoint(music1ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y))
 		{
 			mAudio->PlaySFX("Music/Intoleranzen.mp3", 0, 1);
+
+			/*ToggleButton(music1ButtonTexture, "Buttons.png", 344, 85, 172, 85, 0.75f, 0.5f);
+			ToggleButton(music2ButtonTexture, "Buttons.png", 516, 0, 172, 85, 0.5f, 0.5f);
+			ToggleButton(music3ButtonTexture, "Buttons.png", 688, 0, 172, 85, 0.25f, 0.5f);*/
+
+			music1ButtonTexture = new Texture("Buttons.png", 344, 85, 172, 85);
+			music2ButtonTexture = new Texture("Buttons.png", 516, 0, 172, 85);
+			music3ButtonTexture = new Texture("Buttons.png", 688, 0, 172, 85);
+			music1ButtonTexture->SetParent(iconHolder);
+			music2ButtonTexture->SetParent(iconHolder);
+			music3ButtonTexture->SetParent(iconHolder);
+			music1ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.75f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music2ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music3ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.25f, -Graphics::SCREEN_HEIGHT * 0.5f);
+
 			std::cout << "1\n";
 		}
-		else if (button->ContainsPoint(music2ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y)) 
+		else if (button->ContainsPoint(music2ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y))
 		{
 			mAudio->PlaySFX("Music/reCreation.mp3", 0, 1);
+
+		/*	ToggleButton(music1ButtonTexture, "Buttons.png", 344, 0, 172, 85, 0.75f, 0.5f);
+			ToggleButton(music2ButtonTexture, "Buttons.png", 516, 85, 172, 85, 0.5f, 0.5f);
+			ToggleButton(music3ButtonTexture, "Buttons.png", 688, 0, 172, 85, 0.25f, 0.5f);*/
+
+			//Steve checked and couldn't figure it out
+			music1ButtonTexture = new Texture("Buttons.png", 344, 0, 172, 85);
+			music2ButtonTexture = new Texture("Buttons.png", 516, 85, 172, 85);
+			music3ButtonTexture = new Texture("Buttons.png", 688, 0, 172, 85);
+			music1ButtonTexture->SetParent(iconHolder);
+			music2ButtonTexture->SetParent(iconHolder);
+			music3ButtonTexture->SetParent(iconHolder);
+			music1ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.75f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music2ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music3ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.25f, -Graphics::SCREEN_HEIGHT * 0.5f);
+
 			std::cout << "2\n";
 		}
-		else if (button->ContainsPoint(music3ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y)) 
+		else if (button->ContainsPoint(music3ButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y))
 		{
 			mAudio->PlaySFX("Music/The_Mellotron.mp3", 0, 1);
+
+			music1ButtonTexture = new Texture("Buttons.png", 344, 0, 172, 85);
+			music2ButtonTexture = new Texture("Buttons.png", 516, 0, 172, 85);
+			music3ButtonTexture = new Texture("Buttons.png", 688, 85, 172, 85);
+			music1ButtonTexture->SetParent(iconHolder);
+			music2ButtonTexture->SetParent(iconHolder);
+			music3ButtonTexture->SetParent(iconHolder);
+			music1ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.75f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music2ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.5f);
+			music3ButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.25f, -Graphics::SCREEN_HEIGHT * 0.5f);;
+
 			std::cout << "3\n";
 		}
 		else if (button->ContainsPoint(backButtonTexture, mInput->MousePosition().x, mInput->MousePosition().y))
@@ -93,8 +140,16 @@ void MusicScreen::Update()
 			std::cout << "4\n";
 			//send back to main screen
 		}
-		else 
-		{	
+		else
+		{
 		}
 	}
+}
+
+//Steve checked and couldn't figure it out
+void MusicScreen::ToggleButton(Texture * texture, std::string fileName, int x, int y, int w, int h, float xMod, float yMod) 
+{
+	texture = new Texture(fileName, x, y, w, h);
+	texture->SetParent(iconHolder);
+	texture->SetPosition(-Graphics::SCREEN_WIDTH * xMod, -Graphics::SCREEN_HEIGHT * yMod);
 }
--- PlayfulPicture/PlayfulPicture/MusicScreen.h
+++ PlayfulPicture/PlayfulPicture/MusicScreen.h
@@ -5,10 +5,11 @@
 #include "InputManager.h"
 #include "AudioManager.h"
 #include "Buttons.h"
+#include <string>
 
 using namespace SDLFramework;
 
-class MusicScreen {
+class MusicScreen : public GameEntity{
 
 private:
 	static MusicScreen* sInstance;
@@ -18,12 +19,15 @@ private:
 	AudioManager* mAudio;
 
 	GameEntity* iconHolder;
+	Texture* backgroundTexture;
 	Texture* music1ButtonTexture;
 	Texture* music2ButtonTexture;
 	Texture* music3ButtonTexture;
 	Texture* backButtonTexture;
 
 	Buttons* button;
+
+	void ToggleButton(Texture* texture, std::string fileName, int x, int y, int w, int h, float xMod, float yMod);
 
 public:
 	MusicScreen();
