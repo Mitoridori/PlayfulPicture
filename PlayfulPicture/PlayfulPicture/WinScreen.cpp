b3dbc02f5cec44265249c03bbd77f275cea7bced
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2020-02-07 16:59:09 -0500

    created the win screen
    created the basis for the win screen, just waiting for the assets and a variable from the play screen

--- PlayfulPicture/PlayfulPicture/WinScreen.cpp
+++ PlayfulPicture/PlayfulPicture/WinScreen.cpp
@@ -2,10 +2,83 @@
 
 WinScreen::WinScreen()
 {
+	timer = Timer::Instance();
+	input = InputManager::Instance();
+
+	button = new Buttons();
+
+	topBar = new GameEntity(Graphics::SCREEN_WIDTH, 50.0f);
+	screenLabel = new Texture("You Win!!", "emulogic.ttf", 32, { 255, 0, 111 });
+	topBar->SetParent(this);
+	screenLabel->SetParent(topBar);
+	screenLabel->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, 0.0f);
+
+	movesCounter = new Texture("Total Moves: " /*+ static variable from play*/, "emuloagic.ttf", 32, {255, 0, 111});
+	movesCounter->SetParent(topBar);
+	movesCounter->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.75f);
+
+	buttonHolder = new GameEntity(Graphics::SCREEN_WIDTH, Graphics::SCREEN_HEIGHT);
+	//menuButtonTexture = new Texture("Buttons.jpg", xStart, yStart, xEnd, yEnd);
+	//quitButtonTexture = new Texture("Buttons.jpg", xStart, yStart, xEnd, yEnd);
+
+	backgroundImage = new Texture("Background.jpg", 0, 0, 1760, 1200);
+
+	buttonHolder->SetParent(this);
+	/*menuButtonTexture->SetParent(buttonHolder);
+	quitButtonTexture->SetParent(buttonHolder);*/
+	backgroundImage->SetParent(buttonHolder);
+
+	/*menuButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.2f, -Graphics::SCREEN_HEIGHT * 0.3f);
+	quitButtonTexture->SetPosition(-Graphics::SCREEN_WIDTH * 0.8f, -Graphics::SCREEN_HEIGHT * 0.3f);*/
+	backgroundImage->SetPosition(-Graphics::SCREEN_WIDTH * 0.5f, -Graphics::SCREEN_HEIGHT * 0.5f);
 
 }
 
 WinScreen::~WinScreen()
 {
+	delete topBar;
+	topBar = nullptr;
 
+	delete screenLabel;
+	screenLabel = nullptr;
+
+	delete movesCounter;
+	movesCounter = nullptr;
+
+	delete buttonHolder;
+	buttonHolder = nullptr;
+
+	delete menuButtonTexture;
+	menuButtonTexture = nullptr;
+
+	delete quitButtonTexture;
+	quitButtonTexture = nullptr;
+
+	delete backgroundImage;
+	backgroundImage = nullptr;
+
+	delete button;
+	button = nullptr;
+}
+
+void WinScreen::Render()
+{
+	backgroundImage->Render();
+	screenLabel->Render();
+	movesCounter->Render();
+	/*menuButtonTexture->Render();
+	quitButtonTexture->Render();*/
+}
+
+void WinScreen::Update()
+{
+	if (input->MouseButtonPressed(input->Left)) {
+		if (button->ContainsPoint(menuButtonTexture, input->MousePosition().x, input->MousePosition().y)) {
+			menu = true;
+		}
+		else if (button->ContainsPoint(quitButtonTexture, input->MousePosition().x, input->MousePosition().y)) {
+			quit = true;
+		}
+
+	}
 }
--- PlayfulPicture/PlayfulPicture/WinScreen.h
+++ PlayfulPicture/PlayfulPicture/WinScreen.h
@@ -2,16 +2,36 @@
 #include "Timer.h"
 #include "Texture.h"
 #include "InputManager.h"
+#include "Buttons.h"
+#include "PlayScreen.h"
 
 using namespace SDLFramework;
 
-class WinScreen {
+class WinScreen : public GameEntity {
+
+public:
+	bool menu = false;
+	bool quit = false;
 
 private:
 	Timer* timer;
 	InputManager* input;
 
+	Texture* backgroundImage;
+
+	GameEntity* topBar;
+	Texture* screenLabel;
+	Texture* movesCounter;
+
+	GameEntity* buttonHolder;
+	Texture* menuButtonTexture;
+	Texture* quitButtonTexture;
+
+	Buttons* button;
+
 public:
 	WinScreen();
 	~WinScreen();
+	void Render() override;
+	void Update() override;
 };
