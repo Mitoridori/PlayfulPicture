eeec0937340ca4d175f3cd4724d5916dccab4f6a
StevenCoverdale <S.coverdale@hotmail.com>
2020-02-20 17:29:24 -0500

    Comment For Rebecca
    Co-Authored-By: Rebecca Stewart <beckrpgirl@users.noreply.github.com>

--- PlayfulPicture/PlayfulPicture/Board.cpp
+++ PlayfulPicture/PlayfulPicture/Board.cpp
@@ -142,6 +142,28 @@ namespace SDLFramework {
 
 		More so a good code cleanup.
 		however for optimization you defiantly improved your Invoke calls +1.
+
+		Tile based movement
+		Using the mouse x and y position you'll use Multiplication rounding and division to get the tile.
+
+		say the grid size was 200 you would need to divide the mouse positions x and y by 200. then round that value to the nearest decimal.
+		Next you will want to multiply the result by 200.
+
+		*** Pseudo code ***
+		
+		float GridSize = 100.0f;
+
+		// Get mouse position
+		vector2 position = mousePosition();
+
+		// Calculate Grid Position
+		vector2 tempPosition = vector2(Position / GridSize);
+		vector2 pointInGrid = vector2(round(tempPosition.X), round(tempPosition.Y)) * GridSize;
+
+		//New location in cell -> pointInGrid;
+
+		The last step would be to figure out which index the pointInGrid is located in - using division. (Note: Don't forget to include the offset!)
+
 	*/
 
 	//Rebecca
